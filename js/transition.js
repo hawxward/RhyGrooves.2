9821b1281d78808813533daee33446c3fec48a05
Alex Hawker <malex.leftforchance@gmail.com>
2020-02-24 21:27:23 +0000

    full width

--- js/transition.js
+++ js/transition.js
@@ -4,18 +4,21 @@ import { TimelineLite } from "gsap";
 class Fade extends Highway.Transition {
   in({ from, to, done }) {
     const tl = new TimelineLite();
-    tl.fromTo(to, 1, { right: "-100%", top: "50vh" }, { left: "2%" }).fromTo(
-      to,
-      1,
-      { height: "1vh" },
-      {
-        height: "100vh",
-        top: "0vh",
-        onComplete: function() {
-          done();
+    tl.fromTo(to, 1, { right: "-100%", top: "40vh" }, { left: "0%" })
+      .fromTo(
+        to,
+        1,
+        { height: "1vh" },
+        {
+          height: "100vh",
+          top: "0vh",
+          onComplete: function() {
+            from.remove();
+            done();
+          }
         }
-      }
-    );
+      )
+      .fromTo(to.children[0], 2, { opacity: 0 }, { opacity: 1 });
   }
   out({ from, done }) {
     done();
